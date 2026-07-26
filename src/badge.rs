14e6cfe4800acb22fe91c8004d3e9767d9bf457d
akmal <isakulovdev@gmail.com>
2026-07-26 12:30:45 +0500

    fix: remove unnecessary allocations

--- src/badge.rs
+++ src/badge.rs
@@ -9,46 +9,38 @@ pub async fn generate_badge(instance: String, owner: String, repo: String) -> St
         .await
         .expect("Couldn't read text");
 
-    body = body
-        .replace("\"", "")
-        .replace("\n", "")
-        .replace("{", "")
-        .replace("}", "");
+    body.retain(|c| !matches!(c, '\n' | '"' | '{' | '}'));
 
-    let parts = body.split(",").collect::<Vec<&str>>();
-    // println!("Body: {parts:#?}");
+    let parts = body.split(",");
 
-    let mut table: HashMap<String, u64> = HashMap::new();
-    for part in parts.clone() {
-        let part = part.split(":").collect::<Vec<&str>>();
-        let lang = part
-            .first()
-            .expect("Couldn't extract the language")
-            .to_string();
-        let bytes = part
-            .get(1)
-            .expect("Couldn't extract the bytes")
-            .to_string()
-            .parse::<u64>()
-            .expect("Couldn't parse u64");
-
-        table.insert(lang, bytes);
-    }
-    // println!("Table: {table:#?}");
+    let table = parts
+        .clone()
+        .map(|p| p.split(":"))
+        .map(|mut s| {
+            (
+                s.next().expect("Couldn't extract the language"),
+                s.next()
+                    .expect("Couldn't extract the bytes")
+                    .parse::<u64>()
+                    .expect("Couldn't parse u64"),
+            )
+        })
+        .collect::<Vec<(&str, u64)>>()
+        .into_iter()
+        .collect::<HashMap<&str, u64>>();
 
     let mut total = 0;
     for (_, value) in table.iter() {
         total += value;
     }
 
-    let mut entries = table.clone().into_iter().collect::<Vec<(String, u64)>>();
+    let mut entries = table.clone().into_iter().collect::<Vec<(&str, u64)>>();
     entries.sort_by(|el1, el2| el2.1.cmp(&el1.1));
     let entry = entries.first().unwrap().to_owned();
     let lang = entry.0;
     let bytes = entry.1;
 
     let percentage = ((bytes as f64 / total as f64) * 100f64)
-        // let percentage = ((420308f64 / total as f64) * 100f64)
         .to_string()
         .split(".")
         .next()
@@ -56,13 +48,7 @@ pub async fn generate_badge(instance: String, owner: String, repo: String) -> St
         .parse::<u64>()
         .expect("Couldn't parse the percentage");
 
-    // println!(
-    //     "Language: {}, Bytes: {}, Total: {}, Percentage: {}",
-    //     lang, bytes, total, percentage
-    // );
-
     let link = format!("https://img.shields.io/badge/{lang}-{percentage}%25-blue?logo={lang}");
-    // println!("Link: {link}");
 
     link
 }
